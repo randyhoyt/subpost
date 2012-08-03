1f14b6cb5f4098bccff77969d3b2babb151f19cd
randyhoyt <randy@randyhoyt.com>
2012-08-03 12:59:21 -0400

    Initial commit

---
+++ children.php
@@ -0,0 +1,145 @@
+<?php
+
+require_once('../../../../wp-admin/admin.php');
+
+if ($_REQUEST["action"]=="save") {
+
+	// check nonce
+
+	if ($_POST['form_submit']=="Move to Trash") {
+
+		$post_id = (int) $_POST["ID"]; 
+		if ($post_id > 0) {
+			$post = get_post($post_id);
+		}
+		$result = wp_trash_post($post->ID);		
+
+	} else {
+
+		// save and return with JavaScript
+		$post_id = (int) $_POST["ID"]; 
+		if ($post_id > 0) {
+			$post = get_post($post_id);
+		}
+
+		if($post->ID == 0) {
+			$post = new stdClass;
+			$post->post_status = 'publish';
+		}
+
+		$post->post_title = $_POST["post_title"];
+		$post->post_content = $_POST["post_content"];
+		$post->post_type = $_POST["post_type"];
+		$post->post_parent = $_POST["post_parent"];	
+		$post->ID = wp_insert_post($post);
+
+		// TO DO -- save custom fields
+
+		do_action('subpost_save_form_fields',$post);
+
+	}
+
+?><!DOCTYPE html>
+<html>
+<head>
+	<script>
+		var display_html = '<?php echo str_replace("'","\\\'",subpost_display_all_children($_POST["post_parent"], $post->post_type)); ?>';
+		var win = window.dialogArguments || opener || parent || top;
+		win.subpost_list_children(display_html,"<?php echo $post->post_type; ?>");			
+	</script>
+</head>
+<body>	
+</body>
+</html><?php
+
+	exit();
+
+} else {
+
+	$mode = "add";
+
+	$child = new stdClass;
+	$child->ID = 0;
+
+	$post_id = (int) trim($_REQUEST['post']);
+	if ($post_id > 0) {
+		$child = get_post($post_id);
+	}
+
+	if ($child->ID > 0) {
+		$mode = "edit";
+	} else {
+		$post_parent = (int) $_REQUEST['post_parent'];
+		$child->post_parent = $post_parent;
+		$child->post_type = $_REQUEST['post_type'];	
+	}
+
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo('url') . ('/wp-admin/load-styles.php?c=1&dir=ltr&load=admin-bar,wp-admin,media'); ?>" />	
+	<link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo('url') . ('/wp-admin/css/colors-fresh.css'); ?>" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<style>
+		.deletelink {color: red !important; border: 0 !important; font-weight: normal !important;}
+		.deletelink:hover {color: white !important; background: red; cursor: pointer; text-decoration: underline; }
+	</style>		
+</head>
+<body style="min-height: 0px; height: auto;">
+
+	<?php
+		$form_title = $_REQUEST['form_title'];
+		if ($form_title!="") {
+	?><h3 style="padding-left: 1.25em;" class="media-title"><?php echo $form_title; ?></h3><?php
+		}
+	?>
+
+	<form method="post" class="media-upload-form"><div id="media-items"><div style="margin: 1em;" class="media-item"><div style="padding: 1em;">
+
+		<input type="hidden" name="action" value="save" />
+		<input type="hidden" name="post_parent" value="<?php echo $child->post_parent; ?>" />
+		<input type="hidden" name="post_type" value="<?php echo $child->post_type; ?>" />
+		<?php if ($mode=="edit") { ?><input type="hidden" name="ID" value="<?php echo $child->ID; ?>" /><?php } ?>
+
+		<table class="describe">
+			<tbody style="border: 1px solid white;">
+				<?php // TO DO -- check if it supports the title ?? { ?>				
+				<tr>
+					<th valign="top" scope="row" class="label" style="width:130px;">
+						<span class="alignleft"><label for="src">Title</label></span>
+						<span class="alignright"><abbr id="status_img" title="required" class="required">*</abbr></span>
+					</th>
+					<td class="field"><input type="text" name="post_title" autofocus value="<?php echo esc_attr($child->post_title); ?>"></td>
+				</tr>
+				<?php // } ?>
+				<?php // TO DO -- check if it supports the title ?? { ?>				
+				<tr>
+					<th valign="top" scope="row" class="label" style="width:130px;">
+						<span class="alignleft"><label for="src">Content</label></span>
+						<span class="alignright"></span>
+					</th>
+					<td class="field"><textarea name="post_content"><?php echo $child->post_content; ?></textarea></td>
+				</tr>
+				<?php // } ?>
+
+				<?php echo apply_filters('subpost_form_fields','',$child); ?>
+
+				<tr>
+					<th valign="top" scope="row" class="label" style="width:130px; padding-top: 1.5em;">
+						
+					</th>
+					<td style="padding-top: 1.5em;" class="image-only">
+						<input type="submit" class="button-secondary" value="Save" />
+						<span class="alignright"><?php if ($mode=="edit") { ?><input name="form_submit" type="submit" class="deletelink" value="Move to Trash" /><?php } ?></span>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+
+	</div></div></div></form>
+
+</body>
+</html>
---
+++ subpost.php
@@ -0,0 +1,228 @@
+<?php
+
+/*
+Plugin Name: Subordinate Post Type Helpers
+Description: This plugin provides a number of helpers for registering a custom post type that is subordinate to another post type.
+*/
+
+function register_sub_post_type($post_type,$args,$parent_post_type) {
+	
+	$current_sub_post_type = array (
+			'post_type' => $post_type,
+			'args' => $args,
+			'parent_post_type' => $parent_post_type
+		); 
+
+	register_post_type($post_type,$args);
+
+	$sub_post_type_factory = SubPostTypeFactory::getInstance();
+	$sub_post_type_factory->addPostType($current_sub_post_type);
+	add_action('init',$sub_post_type_factory->wp_init(),99999);
+
+}
+
+class SubPostTypeFactory
+{
+
+	private static $sub_post_type_factory;
+	private static $sub_post_types;
+	private static $sub_post_type_current;
+
+	private function __construct() {}
+
+	public static function getInstance() {
+
+		if (!self::$sub_post_type_factory) {
+			self::$sub_post_type_factory = new SubPostTypeFactory();
+		}
+		return self::$sub_post_type_factory;
+	}
+
+	public static function addPostType($post_type_new) {
+
+		self::$sub_post_types[$post_type_new["post_type"]] = $post_type_new;
+	
+	}
+
+	public function getPostTypes() {
+
+		return self::$sub_post_types;
+
+	}
+
+	public function setCurrentPostType($post_type) {
+
+		self::$sub_post_type_current = $post_type;
+
+	}
+
+	public function getCurrentPostType() {
+
+		return self::$sub_post_type_current;
+
+	}
+
+	public function wp_init() {
+
+		$sub_post_type_factory = self::getInstance();
+		$post_types = $sub_post_type_factory->getPostTypes();
+		
+		if ($post_types) {
+		
+			add_action("admin_menu", "subpost_add_to_submenu",10,2);
+			add_action("add_meta_boxes","subpost_add_meta_box");
+
+		}
+	}
+
+}
+
+function subpost_add_to_submenu($arg1=null, $arg2=null, $arg3=null, $arg4=null) {
+
+	global $menu,$submenu;
+
+
+
+	$sub_post_type_factory = SubPostTypeFactory::getInstance();	
+	$post_types = $sub_post_type_factory->getPostTypes();
+
+	if ($post_types) {
+		foreach ($post_types as $post_type) {
+			add_submenu_page('edit.php?post_type=' . $post_type["parent_post_type"], $post_type['args']['labels']['name'], $post_type['args']['labels']['name'], 'edit_posts', 'edit.php?post_type=' . $post_type["post_type"]);
+			add_submenu_page('edit.php?post_type=' . $post_type["parent_post_type"], $post_type['args']['labels']['all_items'], "&#8212; " . $post_type['args']['labels']['all_items'], 'edit_posts', 'edit.php?post_type=' . $post_type["post_type"]);
+			add_submenu_page('edit.php?post_type=' . $post_type["parent_post_type"], $post_type['args']['labels']['add_new_item'], "&#8212; " . $post_type['args']['labels']['add_new_item'], 'edit_posts', 'post-new.php?post_type=' . $post_type["post_type"]);
+			remove_menu_page('edit.php?post_type=' . $post_type["post_type"]);	
+		}
+	}
+
+//if ()
+//$menu[26][4] .= ' wp-has-current-submenu wp-menu-open'; 
+//echo get_post_type($_REQUEST['post']); die();
+//print_r($menu); die();		
+
+}
+
+function subpost_add_meta_box() {
+
+	global $post;
+	$sub_post_type_factory = SubPostTypeFactory::getInstance();
+	$post_types = $sub_post_type_factory->getPostTypes();
+		
+	if ($post_types) {
+
+		foreach($post_types as $post_type) {
+
+			if ($post->post_type == $post_type["parent_post_type"]) {
+
+				$sub_post_type_factory->setCurrentPostType($post_type["post_type"]);
+
+			   	add_meta_box(
+							'custom_meta_box_' . $post_type["post_type"], 
+							$post_type['args']['labels']['name'], 
+							'subpost_render_meta_box', 
+							$post_type["parent_post_type"], 
+							'normal', 
+							'high'
+					);
+
+			}
+		}
+
+	}
+
+}
+
+function subpost_render_meta_box() {
+
+	global $post;
+
+	$sub_post_type_factory = SubPostTypeFactory::getInstance();
+	$post_types = $sub_post_type_factory->getPostTypes();
+	$current_sub_post_type = $post_types[$sub_post_type_factory->getCurrentPostType()];
+
+	echo '<div id="subpost_list_children_' . $current_sub_post_type["post_type"] . '">';
+	$output = subpost_display_all_children($post->ID,$current_sub_post_type["post_type"]);
+	if ($output) {
+		echo $output;
+	} else {
+		echo '<p>' . $current_sub_post_type["args"]['labels']['not_found'] . '</p>';
+	}
+	echo '</div>';
+	echo '<a title="' . $current_sub_post_type["args"]['labels']['add_new_item'] . '" class="button thickbox" href="'. plugin_dir_url(__FILE__) . 'children.php?&amp;form_title=' . $current_sub_post_type["args"]['labels']['add_new_item'] . '&amp;post_parent=' . $post->ID . '&amp;post_type=' . $current_sub_post_type["post_type"] . '&amp;TB_iframe=1&amp;width=480&amp;height=440">' . $current_sub_post_type["args"]['labels']['add_new_item'] . '</a>';	
+}
+
+function subpost_display_all_children($post_id,$sub_post_type_name) {
+
+	$output = "";
+
+	$sub_post_type_factory = SubPostTypeFactory::getInstance();
+	$post_types = $sub_post_type_factory->getPostTypes();
+	$sub_post_type = $post_types[$sub_post_type_name];	
+
+	$subposts = get_posts(array(
+				'post_type' => $sub_post_type["post_type"],
+				'post_parent' => $post_id,
+				'numberposts' => -1,
+				'order' => 'ASC'
+			));
+	if ($subposts) {
+
+		$output .= '<table class="wp-list-table widefat fixed posts" style="margin: 12px 0; border-bottom: 0;" cellspacing="0"><tbody id="the-list">';
+		
+		foreach($subposts as $subpost) {
+			$output .= subpost_display_one_child($subpost,$sub_post_type);
+		}
+
+		$output .= '</tbody></table>';
+
+		return $output;
+
+	} else {
+		return false;
+	}
+
+}
+
+function subpost_display_one_child($post,$sub_post_type) {
+
+	$output = "";
+
+	$edit_link = plugin_dir_url(__FILE__) . 'children.php?&amp;form_title=' . $sub_post_type["args"]['labels']['edit_item'] . '&amp;post=' . $post->ID . '&amp;post_parent=' . $post->post_parent . '&amp;post_type=' . $sub_post_type["post_type"] . '&amp;TB_iframe=1&amp;width=480&amp;height=440';
+	//$output .= '<p>';
+	//$output .= $post->post_title;
+	//$output .= 	'<br /><a title="' . $sub_post_type["args"]['labels']['add_new_item'] . '" class="thickbox" href="'. plugin_dir_url(__FILE__) . 'children.php?&amp;post=' . $post->ID . '&amp;post_parent=' . $post->post_parent . '&amp;post_type=' . $sub_post_type["post_type"] . '&amp;TB_iframe=1&amp;width=480&amp;height=440">' . $sub_post_type["args"]['labels']['edit_item'] . '</a>';
+	//$output .= '</p>';
+	$output .= '<tr id="post-' . $post->ID . '" class="post-' . $post->ID . '" valign="top">';
+	$output .= '<td class="post-title page-title column-title"><strong><a class="row-title thickbox" href="' . $edit_link . '" title="' . $sub_post_type["args"]['labels']['edit_item'] . '">' . esc_attr($post->post_title) . '</a></strong>';
+	$output .= '<span class="row-actions"><span class="edit"><a class="thickbox" href="' . $edit_link . '" title="' . $sub_post_type["args"]['labels']['edit_item'] . '">' . $sub_post_type["args"]['labels']['edit_item'] . '</a></span></td>';
+
+	$output .= apply_filters("subpost_display_column","",$post,$sub_post_type);
+
+	$output .= "</tr>";
+
+	// apply global filter
+	// apply post_type specific filter to output
+
+	//$output = '<li id="' . $post->post_type . '-' . $post->ID . '">' . $output . '</li>';
+
+	return $output;
+
+}
+
+add_action( 'admin_footer', 'subpost_javascript_footer');
+function subpost_javascript_footer() {
+
+?>
+
+	<script>
+		
+		function subpost_list_children(html_display,sub_post_type) {			
+			jQuery('#subpost_list_children_' + sub_post_type).empty().append(html_display);			
+			tb_remove();
+		}
+
+	</script>
+	
+<?php
+	
+}
