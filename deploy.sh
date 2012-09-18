7553b3837f44c9796ed9189af8e15aa75164edf0
Randy Hoyt <randy@randyhoyt.com>
2012-09-18 16:57:53 +0000

    Updating deploy script

---
+++ deploy.sh
@@ -0,0 +1,56 @@
+#! /bin/bash
+#
+# Script to deploy from Github to WordPress.org Plugin Repository
+# A modification of Dean Clatworthy's deploy script as found here: https://github.com/deanc/wordpress-plugin-git-svn
+# The difference is that this script lives in the plugin's git repo & doesn't require an existing SVN repo.
+# Source: https://github.com/thenbrent/multisite-user-management/blob/master/deploy.sh
+
+# Configure these values for each plugin.
+GITSLUG="subpost"
+MAINFILE="subpost.php"
+SVNSLUG="subordinate-post-type-helpers"
+SVNUSER="randyhoyt"
+
+# ###### Do not modify below this point. ######
+
+# Set up Git repository configuration.
+GITPATH=`pwd`
+GITFOLDER='plugins/'$GITSLUG
+
+# Check the version numbers and exit if they don't match.
+NEWVERSIONTXT=`grep "^Stable tag" $GITPATH/readme.txt | awk -F' ' '{print $3}'`
+NEWVERSIONPHP=`grep "^Version" $GITPATH/$MAINFILE | awk -F' ' '{print $2}'`
+#if [ "$NEWVERSIONPHP" != 9 ]; then echo "Versions don't match. Please try again."; exit 1; fi
+
+# Tag this version in Git.
+#echo "Versions match in readme.txt and PHP file. Let's proceed."
+echo "Tagging new version in Git."
+git tag -a "$NEWVERSIONTXT" -m "Tagging version $NEWVERSIONTXT"
+
+echo "Pushing latest commit to origin, with tags."
+git push origin master
+git push origin master --tags
+
+# Set up Subversion repository configuration.
+SVNFOLDER='svn/'$SVNSLUG
+SVNPATH=${GITPATH/$GITFOLDER/$SVNFOLDER}
+SVNURL="http://plugins.svn.wordpress.org/$SVNSLUG"
+
+# Remove any folders in the current Subversion folder and checkout the repository afresh.
+rm -r -f $SVNPATH
+
+echo "Creating local copy of the Subversion repository."
+svn co $SVNURL $SVNPATH
+
+echo "Ignoring github specific files and deployment script."
+svn propset svn:ignore "deploy.sh
+README.md
+.git
+.gitignore" "$SVNPATH/trunk/"
+
+echo "Exporting from Git to Subversion trunk."
+git checkout-index -a -f --prefix=$SVNPATH/trunk/
+
+echo "Switching to Subversion directory and committing."
+cd $SVNPATH/trunk/
+svn commit --username=$SVNUSER -m "Committing version $NEWVERSIONTXT"
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: randyhoyt
 Tags: custom post types
 Requires at least: 3.4.1
 Tested up to: 3.4.1f
-Stable tag: 0.1.1
+Stable tag: 0.1.1 
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
