5397a69712169757491c80abfa5112ef5f3d5375
Randy Hoyt <randy@randyhoyt.com>
2012-09-24 15:05:44 +0000

    Updating deploy script

--- deploy.sh
+++ deploy.sh
@@ -28,8 +28,10 @@ read VERSION_NUMBER
 echo "Tagging new version in Git."
 git checkout master
 git merge dev
-sed -c -ie 's/VERSION/0.1.1d/g' ${GITPATH}/readme.txt
-sed -c -ie 's/VERSION/0.1.1d/g' ${GITPATH}/${GITSLUG}.php
+git add *
+git commit -m "Merging version $VERSION_NUMBER to master"
+sed 's/VERSION/Yeah/g' ${GITPATH}/readme.txt
+sed 's/VERSION/0.1.1d/g' ${GITPATH}/${GITSLUG}.php
 
 git tag -a "$VERSION_NUMBER" -m "Tagging version $VERSION_NUMBER"
 git push
